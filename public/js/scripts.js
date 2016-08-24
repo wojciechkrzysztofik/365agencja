04d4319f63c146fddb2251089e9244b4784e8c08
Wojciech Krzysztofik <Wojciech Krzysztofik>
2016-08-24 20:33:46 +0200

    Few new sections.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,6 +6,11 @@ $(function() {
 
       this.mainMenu = $('.navbar-default');
       this.scrollToTopBtn = $('.up');
+      this.homeBtn = $('.navbar-brand');
+      this.omnichannelBtn = $('#btn-omnichannel');
+      this.warrantyBtn = $('#btn-warranty');
+      this.contactUsBtn = $('#btn-contact-us');
+      this.contactBtn = $('.btn-contact');
 
       this.init = function() {
 
@@ -15,7 +20,7 @@ $(function() {
         var topOffset = $(window).scrollTop();
         var alternativeTheme = 'navbar-scrolled';
 
-        if($(window).width() > 769) {
+        if($(window).width() > 992) {
             if(topOffset > 0) {
                 this.mainMenu.addClass(alternativeTheme, 1000);
             } else {
@@ -26,10 +31,50 @@ $(function() {
         }
       };
 
+      this.setActiveMenuItem = function(element) {
+        $('.navbar-nav a').removeClass('active');
+        element.addClass('active');
+      };
+
       this.scrollToTopBtn.on('click', function(e) {
           e.preventDefault();
 
           $('html, body').animate({scrollTop : 0}, 800);
+      });
+
+      this.homeBtn.on('click', function(e) {
+          e.preventDefault();
+
+          $('.navbar-nav a').removeClass('active');
+          $('html, body').animate({scrollTop : 0}, 800);
+      });
+
+      this.omnichannelBtn.on('click', function(e) {
+          e.preventDefault();
+
+          $('html, body').animate({scrollTop : $('.omnichannel').offset().top - 70}, 800);
+          this.setActiveMenuItem(this.omnichannelBtn);
+      });
+
+      this.warrantyBtn.on('click', function(e) {
+          e.preventDefault();
+
+          $('html, body').animate({scrollTop : $('.warranty').offset().top - 70}, 800);
+          this.setActiveMenuItem(this.warrantyBtn);
+      });
+
+      this.contactUsBtn.on('click', function(e) {
+          e.preventDefault();
+
+          $('html, body').animate({scrollTop : $('.contact-us').offset().top - 70}, 800);
+          this.setActiveMenuItem(this.contactUsBtn);
+      });
+
+      this.contactBtn.on('click', function(e) {
+          e.preventDefault();
+
+          $('html, body').animate({scrollTop : $('.contact-us').offset().top - 70}, 800);
+          this.setActiveMenuItem(this.contactUsBtn);
       });
 
     }
